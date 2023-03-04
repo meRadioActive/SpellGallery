0ce3a9ca8f2f796703ac2f4db0b077049ddb31f4
valters <valters.zilinskis@gmail.com>
2023-03-04 00:05:13 +0000

    Spell stuff...

---
+++ src/renderer/src/components/SpellInfo.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { inject, reactive } from 'vue';
+const show = reactive(inject('showSpellInfo'))
+console.log(show)
+</script>
+
+<template>
+  <div class="modal fade" v-if="show">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="spellInfoTitle"></h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-4">Level:</div>
+                <div class="col-md-4"><slot name="level" /></div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">School:</div>
+                <div class="col-md-4"><slot name="school" /></div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Casting Time:</div>
+                <div class="col-md-4"><slot name="castingTime" /></div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Range:</div>
+                <div class="col-md-4"><slot name="range" /></div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Duration:</div>
+                <div class="col-md-4"><slot name="duration" /></div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Book:</div>
+                <div class="col-md-4"><slot name="book" /></div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Page:</div>
+                <div class="col-md-4"><slot name="page" /></div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Class:</div>
+                <div class="col-md-4"><slot name="playerClass" /></div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Components:</div>
+                <div class="col-md-4"><slot name="spellComponents" /></div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  </div>
+</template>
--- src/renderer/src/components/SpellList.vue
+++ src/renderer/src/components/SpellList.vue
@@ -3,6 +3,9 @@ import {useSpellsStore} from "../stores/spells";
 
 
 const spellStore = useSpellsStore();
+let showSpellInfo = false;
+let spellData = spellStore.spellList[0];
+
 function spellClass(data){
   const classList = ["Druid", "Ranger", "Sorcerer", "Wizard", "Cleric", "Paladin", "Warlock", "Bard"];
   let badges = [];
@@ -27,12 +30,19 @@ function componentList(data){
   })
   return components
 }
+function showSpell(spell){
+  spellData = spell;
+  if(spell !== undefined){
+    const myModal = new bootstrap.Modal('#spellInfoData', {focus: true})
+  }
+  console.log(`Showing spell (${showSpellInfo}) with Data: \/n ${spellData}`)
+}
 
 </script>
 
 <template>
   <div class="row pt-2 spellGallery align-items-baseline">
-    <div class="col-lg-3" v-for="spell in spellStore.spellList">
+    <div class="col-lg-3" @click="showSpell(spell)" v-for="spell in spellStore.spellList">
       <div class="card mb-3 spellCard">
         <div class="card-header">
           <div class="row">
@@ -48,6 +58,59 @@ function componentList(data){
           <div class="card-title">Duration: {{ spell.Duration }}</div>
           <div class="card-title" v-if="spell.DamageType !== undefined">DamageType: {{ spell.DamageType }}</div>
           <div class="card-text"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal" tabindex="-1" id="spellInfoData">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="spellInfoTitle"></h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-4">Level:</div>
+                <div class="col-md-4">{{ spellData.Level }}</div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">School:</div>
+                <div class="col-md-4">{{ spellData.School }}</div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Casting Time:</div>
+                <div class="col-md-4">{{ spellData.CastingTime }}</div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Range:</div>
+                <div class="col-md-4">{{ spellData.Range }}</div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Duration:</div>
+                <div class="col-md-4">{{ spellData.Duration }}</div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Book:</div>
+                <div class="col-md-4">{{ spellData.Book }}</div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Page:</div>
+                <div class="col-md-4">{{ spellData.Page }}</div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Class:</div>
+                <div class="col-md-4"><span v-for="playerclass in spellClass(spellStore.spellList.indexOf(spellData))" class="badge text-bg-success mx-1">{{ playerclass }}</span></div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Components:</div>
+                <div class="col-md-4"><span v-for="component in componentList(spellStore.spellList.indexOf(spellData))" class="badge text-bg-info mx-1">{{ component }}</span></div>
+              </div>
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
